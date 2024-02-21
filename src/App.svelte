1c87e050f241613962db985a778f579b588d53b2
10038_jatinoco_L00406001 <jatinoco@espe.edu.ec>
2024-02-20 20:32:24 -0600

    Jeimmy Tinoco

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,25 @@
 <script>
-  let items = ['Manzanas', 'Bananas', 'Naranjas'];
+  let items = [
+    { name: 'Manzanas', completed: false },
+    { name: 'Bananas', completed: false },
+    { name: 'Naranjas', completed: false }
+  ];
+
   let newItem = '';
 
   function addItem() {
     if (newItem.trim() !== '') {
-      items = [...items, newItem];
+      items = [...items, { name: newItem, completed: false }];
       newItem = '';
     }
   }
 
   function removeItem(index) {
     items = items.filter((_, i) => i !== index);
+  }
+
+  function toggleComplete(index) {
+    items[index].completed = !items[index].completed;
   }
 </script>
 
@@ -21,6 +30,20 @@
 
 <ul>
   {#each items as item, index}
-    <li>{item} <button on:click={() => removeItem(index)}>Remove</button></li>
+    <li>
+      <span style="text-decoration: {item.completed ? 'line-through' : 'none'}">{item.name}</span>
+      <button on:click={() => toggleComplete(index)}>{item.completed ? 'Desmarcar' : 'Completar'}</button>
+      <button on:click={() => removeItem(index)}>Remove</button>
+    </li>
+  {/each}
+</ul>
+
+<h1>Completed Items</h1>
+
+<ul>
+  {#each items as item, index}
+    {#if item.completed}
+      <li style="text-decoration: line-through;">{item.name}</li>
+    {/if}
   {/each}
 </ul>
